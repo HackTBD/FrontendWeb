da25258b3a24a222a0331087c184d15442df1f14
TonyNguyenVn17 <17thien.nguyen@gmail.com>
2025-03-20 11:14:52 -0400

    save local layout change before switch branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { AuroraBackground } from './_components/ui/aurora_background';
-import "@radix-ui/themes/styles.css";
-import { Button } from './_components/ui/button';
+import { AuroraBackground } from './_components/ui/AuroraBackground';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
