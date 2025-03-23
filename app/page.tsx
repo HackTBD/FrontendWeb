54cd34e07041d912141f5609444e856bddc167d2
TonyNguyenVn17 <17thien.nguyen@gmail.com>
2025-03-23 14:01:40 -0400

    Landing page
    create light theme for landing page with toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import Link from 'next/link';
 import LandingPage from './landing/LandingPage';
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-black">
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-black">
       <LandingPage />
+      
+      {/* Light theme toggle button */}
+      <div className="fixed bottom-4 right-4 z-50">
+        <Link href="/light_landing">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded-full shadow-lg flex items-center space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
+            </svg>
+            <span>Try Light Theme</span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
